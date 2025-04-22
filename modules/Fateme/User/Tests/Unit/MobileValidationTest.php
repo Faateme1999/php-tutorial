eac1697be65838198949bbf334c2cb71f7bd0211
nmortezamt <mortezanmt30@gmail.com>
2025-04-22 20:45:36 +0330

    minor fix on MobileValidationTest

--- modules/Fateme/User/Tests/Unit/MobileValidationTest.php
+++ modules/Fateme/User/Tests/Unit/MobileValidationTest.php
@@ -12,11 +12,15 @@ class MobileValidationTest extends TestCase
      */
     public function test_mobile_can_not_be_less_than_10_character()
     {
+        $failedMessage = null;
+
         $fail = function (string $message) use (&$failedMessage) {
             $failedMessage = $message;
         };
-       $result = (new validMobile())->validate(attribute:'',value:'', fail: $fail);
-       $this->assertEquals(0,$result);
+
+        (new validMobile())->validate('', '912345671', $fail);
+
+        $this->assertNotNull($failedMessage);
     }
 
     public function test_mobile_can_not_be_more_than_10_character()
